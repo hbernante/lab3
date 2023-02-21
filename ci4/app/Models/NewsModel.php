d66f028c88f3201d614c4cbe2fdee5b406991471
hbernante <119903632+hbernante@users.noreply.github.com>
2023-02-21 09:10:11 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -6,7 +6,7 @@ use CodeIgniter\Model;
 
 class NewsModel extends Model
 {
-    protected $table = 'news';
+    protected $table = 'hbernante_news';
 	
 	    public function getNews($slug = false)
     {
