562b220da2e361622bfc259709763bad68012b4f
hbernante <119903632+hbernante@users.noreply.github.com>
2023-03-07 09:57:08 +0800

    Update NewsModel.php

--- ci4/app/Models/NewsModel.php
+++ ci4/app/Models/NewsModel.php
@@ -9,12 +9,12 @@ class NewsModel extends Model
 
     protected $table = 'news';
 	protected $allowedFields = ['title', 'slug', 'body'];
-		public function getNews ($slug = false)
-		{
-			if ($slug === false) {
-				return $this->findAll();
-			}
-			return $this->where(['slug' => $slug])->first();
-		}
-	
+	   public function getNews($slug = false)
+    {
+        if ($slug === false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug' => $slug])->first();
+    }
 }
